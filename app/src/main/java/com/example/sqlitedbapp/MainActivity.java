deeae41ab90a75e9831866a0aff710ceab44aa59
ElazarusNicoy <aguisandaej@gmail.com>
2024-11-15 05:23:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sqlitedbapp/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.sqlitedbapp;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etStdnId, etStdnName, etStdnProg;
+    Button btAdd, btDelete, btSearch, btView;
+    SQLiteDatabase db;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etStdnId = findViewById(R.id.txtID);
+        etStdnName = findViewById(R.id.txtName);
+        etStdnProg = findViewById(R.id.txtProgram);
+
+        btAdd = findViewById(R.id.btnAdd);
+        btDelete = findViewById(R.id.btnDelete);
+        btSearch = findViewById(R.id.btnSearch);
+        btView = findViewById(R.id.btnViewAll);
+
+        btAdd.setOnClickListener(this);
+        btDelete.setOnClickListener(this);
+        btSearch.setOnClickListener(this);
+        btView.setOnClickListener(this);
+
+        db = openOrCreateDatabase("StudentDB", Context.MODE_PRIVATE, null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS student(stdnt_id VARCHAR PRIMARY KEY, stdnt_name VARCHAR NOT NULL, stdnt_program VARCHAR NOT NULL)");
+
+        etStdnId.requestFocus();
+
+    }
+
+    public void showMessage(String title, String message) {
+        Builder builder = new Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    public void clearText() {
+        etStdnId = (findViewById(R.id.txtID));
+        etStdnId.getText().clear();
+        etStdnName = (findViewById(R.id.txtName));
+        etStdnName.getText().clear();
+        etStdnProg = (findViewById(R.id.txtProgram));
+        etStdnProg.getText().clear();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btAdd) {
+            db.execSQL("INSERT INTO student VALUES('" + etStdnId.getText() + "','" + etStdnName.getText() + "','" + etStdnProg.getText() + "');");
+            showMessage("Success", "Record Added.");
+            clearText();
+        } else if (v == btDelete) {
+            Cursor c = db.rawQuery("DELETE FROM student WHERE stdnt_id= '" + etStdnId.getText() + "'", null);
+            if (c.moveToFirst()) {
+                db.execSQL("DELETE FROM student WHERE stdnt_id= '" + etStdnId.getText() + "'");
+                showMessage("Success", "Record Deleted.");
+            }
+            clearText();
+        } else if (v == btSearch) {
+            Cursor c = db.rawQuery("SELECT * FROM student WHERE stdnt_id= '" + etStdnId.getText() + "'", null);
+            StringBuffer buffer = new StringBuffer();
+            if (c.moveToFirst()) {
+                buffer.append("Name: " + c.getString(1) + "\n");
+                buffer.append("Program: " + c.getString(2) + "\n\n");
+            }
+            showMessage("Student Details", buffer.toString());
+        } else if (v == btView) {
+            Cursor res = db.rawQuery("SELECT * FROM student", null);
+            if (res.getCount() == 0) {
+                showMessage("ERROR", "No records found.");
+            }
+            StringBuffer buffer = new StringBuffer();
+            while (res.moveToNext())
+            {
+                buffer.append("ID: "+res.getString(0)+"\n");
+                buffer.append("Name: "+res.getString(1)+"\n");
+                buffer.append("Program: "+res.getString(2)+"\n\n");
+            }
+            showMessage("Student Details", buffer.toString());
+        }
+    }
+}
